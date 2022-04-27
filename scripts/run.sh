5345d0a6a937a449a5ce8c1468da68815c996ed6
Hanns Holger Rutz <contact@sciss.de>
2022-04-27 16:16:48 +0200

    ... <>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 cd ..
-java -Dsun.java2d.opengl=true -cp common/Rogues-common.jar de.sciss.rogues.SwapRogue --margin 0 --full-screen
+java -Dsun.java2d.opengl=true -cp common/Rogues-common.jar de.sciss.rogues.SwapRogue --margin 0 --full-screen "$@"
